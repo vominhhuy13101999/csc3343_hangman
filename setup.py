eddb0b48319a7043d0b1897982c70946c1074c8c
dsb10503 <dsb10503@univ.fairfield.local>
2021-12-08 14:31:40 -0500

    add requirement for set up

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 from distutils.core import setup, Extension
+import setuptools 
+
 setup(name='search', version='1.0',  \
       ext_modules=[Extension('search', ['python.cpp'])])
 
 # setup(name='spam', version='1.0',  \
 #       ext_modules=[Extension('spam', ['hello.cpp'])])
+
+setuptools.setup(
+    name='hangman',
+    version='0.0.1',
+    # description='hang man game',
+    # author='Huy Vo',
+    # author_email='huy.vo@student.fairfield.edu',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        # 'wheel',
+        'pandas',
+        'pygame',
+        
+        
+    ],
+)
